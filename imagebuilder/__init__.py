johnsonm/flimage

commit 2d0e9fcecd3e57215d42a5c128654c48de787aba

handle unreliable kpartx -d
The kpartx -d command does not reliably remove mappings.  If
it fails, fall back to dmsetup and losetup to remove the
mappings.

Signed-off-by: Michael K Johnson <johnsonm@foresightlinux.org>

Fixes #19

imagebuilder/__init__.py
@@ -23,8 +23,10 @@ import sys
 import tempfile
 
 from plumbum import FG, BG, local
-from plumbum.cmd import bootman, chroot, conary, cp, dd, depmod, dracut
-from plumbum.cmd import extlinux, kpartx, mount, parted, sh, tar, umount
+from plumbum.cmd import bootman, chroot, conary, cp
+from plumbum.cmd import dd, depmod, dmsetup, dracut
+from plumbum.cmd import extlinux, kpartx, losetup, mount
+from plumbum.cmd import parted, sh, tar, umount
 from plumbum.cmd import echo, sqlite3
 import plumbum.version
 
@@ -45,6 +47,7 @@ class ImageBuilder(object):
                                           suffix='.img',
                                           dir=basedir)
         self.mountDevice = self.image
+        self.loopDevices = []
         self.conaryDbMounted = False
         os.close(fd)
         self.rootdir = None
@@ -101,14 +104,21 @@ class ImageBuilder(object):
         if lines:
             self.mountDevice = '/dev/mapper/%s' %(
                 [x.split()[2] for x in lines if x][0])
-            return [self.mountDevice]
-        return []
+            self.loopDevices.append(self.mountDevice)
 
     def createFilesystem(self):
         return self.run(local['mkfs.%s' %self.fstype]['-F', '-L', '/', self.mountDevice])
 
     def unloopImage(self):
-        self.run(kpartx['-d', self.image])
+        if self.loopDevices:
+            self.run(kpartx['-d', self.image])
+            # in case kpartx has failed for any reason, remove the mappings
+            for device in self.loopDevices:
+                if os.path.exists(device):
+                    self.run(dmsetup['remove', device])
+                # /dev/mapper/loop0p1 -> /dev/loop0
+                base = device.replace('/mapper', '')[:-2]
+                self.run(losetup['-d', base])
 
     def mountFilesystem(self):
         self.rootdir = tempfile.mkdtemp(prefix='mkd.', dir=self.basedir)

bin/flimage
@@ -114,10 +114,9 @@ def main(argv):
     try:
         IB.allocateImage(args.sparse)
 
-        loopDevices = []
         if args.type in (('rawHd'),):
             IB.partitionImage(args.size)
-            loopDevices.extend(IB.loopImage())
+            IB.loopImage()
 
         IB.createFilesystem()
 
@@ -183,8 +182,7 @@ def main(argv):
 
         IB.unmountFilesystem()
 
-        if loopDevices:
-            IB.unloopImage()
+        IB.unloopImage()
 
         IB.removeRootdir()
 

commit 036de4bed792524c6f224923e144f6aaf52b65d5

losetup -d only if the loop device is still set up
Commit 2d0e9fcecd3e57215d42a5c128654c48de787aba introduced a workaround
for kpartx -d failing to correctly remove partition mappings.  However,
when kpartx -d functions correctly, losetup -d fails.

Now, calls losetup -d only if the backing_file map exists.

Signed-off-by: Michael K Johnson <johnsonm@foresightlinux.org>

Fixes #21

imagebuilder/__init__.py
@@ -133,7 +133,10 @@ class ImageBuilder(object):
                     self.run(dmsetup['remove', device])
                 # /dev/mapper/loop0p1 -> /dev/loop0
                 base = device.replace('/mapper', '')[:-2]
-                self.run(losetup['-d', base])
+                dev = os.path.basename(base)
+                backing = '/sys/block/' + dev + '/loop/backing_file'
+                if os.path.exists(backing):
+                    self.run(losetup['-d', base])
 
     def mountFilesystem(self):
         self.rootdir = tempfile.mkdtemp(prefix='mkd.', dir=self.basedir)